29d9a59a84dd964fa93d001be5b226ef6eea0ec4
4794-sourse <2456170006@qq.com>
2024-03-14 21:42:57 +0800

    Site updated: 2024-03-14 21:42:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/12/hello-world/","2024/03/12/hello-x64/"];function toRandomPost(){
+var posts=["2024/03/12/hello-x64/","2024/03/12/hello-world/","2024/03/14/tags/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
